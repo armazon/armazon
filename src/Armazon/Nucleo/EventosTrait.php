756d9a6c1ca15a3ff80f5437498fe8a3fac39f33
Nestor Picado <info@nestorpicado.com>
2016-05-03 21:26:33 -0600

    Mejoramos funcionalidad del trait para manejar eventos.

--- src/Armazon/Nucleo/EventosTrait.php
+++ src/Armazon/Nucleo/EventosTrait.php
@@ -6,6 +6,11 @@ trait EventosTrait
 {
     private $eventos = [];
 
+    public function obtenerEventos()
+    {
+        return $this->eventos;
+    }
+
     public function registrarEvento($nombre, callable $definicion, $encadenar = false)
     {
         if (!$encadenar || !isset($this->eventos[$nombre])) {
@@ -15,16 +20,25 @@ trait EventosTrait
         $this->eventos[$nombre][] = $definicion;
     }
 
-    public function ejecutarEvento($nombre, array $argumentos = [], $eliminarAlEjecutar = false)
+    public function anularEvento($nombre)
     {
+        unset($this->eventos[$nombre]);
+    }
+
+    public function ejecutarEvento($nombre, array $argumentos = [])
+    {
+        $resultado = [];
+
         if (isset($this->eventos[$nombre])) {
             foreach ($this->eventos[$nombre] as $definicion) {
-                call_user_func_array($definicion, $argumentos);
-            }
-
-            if ($eliminarAlEjecutar) {
-                unset($this->eventos[$nombre]);
+                $resultado[] = call_user_func_array($definicion, $argumentos);
             }
         }
+
+        if (count($resultado) == 1) {
+            return $resultado[0];
+        }
+
+        return $resultado;
     }
 }
