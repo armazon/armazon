4a41a75402462dc20479ba40eddfd74580994669
Nestor Picado <info@nestorpicado.com>
2016-05-07 17:48:05 -0600

    Mejora de sintáxis y funcionalidad en interfaz para adaptadores de sesiones.

--- src/Armazon/Sesion/AdaptadorInterface.php
+++ src/Armazon/Sesion/AdaptadorInterface.php
@@ -15,51 +15,53 @@ interface AdaptadorInterface
     public function __construct(array $conf);
 
     /**
-     * Cierra la sesión
+     * Cierra la sesión.
      *
      * @return bool
      */
     public function cerrar();
 
     /**
-     * Destruye una sesión
+     * Destruye una sesión.
      *
-     * @param string $id El identificador de sesión que será destruido.
+     * @param string $id  El identificador de sesión que será destruido.
      *
      * @return bool
      */
     public function destruir($id);
 
     /**
-     * Limpia viejas sesiones
+     * Limpia viejas sesiones.
      *
-     * @param int $tiempoVida Las sesiones que no han actualizado en los últimos segundos de $tiempoVida serán eliminados.
+     * @param int $tiempoVida  Las sesiones que no han actualizado en los últimos segundos de $tiempoVida serán eliminados.
      *
      * @return bool
      */
     public function gc($tiempoVida);
 
     /**
-     * Inicializa sesión
+     * Inicializa sesión abriendo la conexión al adaptador.
+     *
+     * @param string $nombre  Nombre de la sesión a inicializar
      *
      * @return bool
      */
-    public function inicializar();
+    public function abrir($nombre);
 
     /**
      * Recupera los datos de la sesión.
      *
-     * @param string $id El identificador de sesión.
+     * @param string $id  El identificador de sesión.
      *
      * @return array
      */
     public function leer($id);
 
     /**
-     * Escribe los datos de sesión
+     * Escribe los datos de sesión.
      *
-     * @param string $id El identificador de sesión.
-     * @param array $datos Los datos de la sesión.
+     * @param string $id  El identificador de sesión.
+     * @param array $datos  Los datos de la sesión.
      *
      * @return bool
      */
