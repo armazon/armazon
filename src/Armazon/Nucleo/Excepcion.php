835b74a610dd316e04742454591fbe970d04f148
Nestor Picado <info@nestorpicado.com>
2016-05-09 21:42:23 -0600

    Nueva excepción con más funcionalidad.

---
+++ src/Armazon/Nucleo/Excepcion.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Armazon\Nucleo;
+
+/**
+ * Clase que agrega más funcionalidad a la excepcion normal.
+ */
+class Excepcion extends \Exception
+{
+    protected $detalle;
+
+    public function __construct($mensaje, $detalle = null, $codigo = 0, \Exception $previous = null)
+    {
+        $this->detalle = $detalle;
+
+        parent::__construct($mensaje, $codigo, $previous);
+    }
+
+    /**
+     * @return bool
+     */
+    final public function tieneDetalle()
+    {
+        return empty($this->detalle);
+    }
+
+    final public function obtenerDetalle()
+    {
+        return $this->detalle;
+    }
+}
